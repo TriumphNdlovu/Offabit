a3cc123e8fc62aa9bc3205f5d493efdf88e417f4
TriumphNdlovu <realtriumphndlovu@gmail.com>
2024-04-30 18:58:44 +0200

    still testing

--- app/Pages/viewOffer/page.tsx
+++ app/Pages/viewOffer/page.tsx
@@ -82,7 +82,8 @@ export default function Viewoffer() {
 <form className="flex px-5 border flex-col lex items-center justify-center" onSubmit={handleSubmit}>
   <div className="p-5 w-9/12 bg-sky-950">
 
-        
+       
+
   {isPostOwner ? 
   (
     <div className="flex justify-between text-white">
